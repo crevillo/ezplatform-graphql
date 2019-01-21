bdunogier/ezplatform-graphql-bundle

Implemented relay support
All content list fields will yield a Connection instead of a list of items.
Options to sort and limit the results are now supported:

```
{
  content {
    blogPosts(sortBy: DateModified, last: 5) {
      edges {
        node {
          title
        }
      }
    }
  }
}
```

GraphQL/Relay/NodeResolver.php
@@ -1,0 +1,75 @@
+<?php
+
+namespace BD\EzPlatformGraphQLBundle\GraphQL\Relay;
+
+use BD\EzPlatformGraphQLBundle\DomainContent\NameHelper;
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use Overblog\GraphQLBundle\Relay\Node\GlobalId;
+use Overblog\GraphQLBundle\Resolver\TypeResolver;
+
+class NodeResolver
+{
+    /**
+     * @var ContentService
+     */
+    private $contentService;
+
+    /**
+     * @var TypeResolver
+     */
+    private $typeResolver;
+
+    /**
+     * @var ContentTypeService
+     */
+    private $contentTypeService;
+    /**
+     * @var NameHelper
+     */
+    private $nameHelper;
+
+    public function __construct(ContentService $contentService, TypeResolver $typeResolver, ContentTypeService $contentTypeService, NameHelper $nameHelper)
+    {
+        $this->contentService = $contentService;
+        $this->typeResolver = $typeResolver;
+        $this->contentTypeService = $contentTypeService;
+        $this->nameHelper = $nameHelper;
+    }
+
+    /**
+     * @param $globalId
+     *
+     * @return null|\eZ\Publish\API\Repository\Values\Content\ContentInfo
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function resolveNode($globalId)
+    {
+        $params = GlobalId::fromGlobalId($globalId);
+
+        if (in_array($params['type'], ['Content', 'DomainContent'])) {
+            return $this->contentService->loadContentInfo($params['id']);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $object
+     *
+     * @return \GraphQL\Type\Definition\Type
+     */
+    public function resolveType($object)
+    {
+        if ($object instanceof ContentInfo) {
+            return $this->typeResolver->resolve(
+                $this->nameHelper->domainContentName(
+                    $this->contentTypeService->loadContentType($object->contentTypeId)
+                )
+            );
+        }
+    }
+}

GraphQL/Relay/SearchResolver.php
@@ -1,0 +1,70 @@
+<?php
+namespace BD\EzPlatformGraphQLBundle\GraphQL\Relay;
+
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
+use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
+use Overblog\GraphQLBundle\Relay\Connection\Output\ConnectionBuilder;
+
+class SearchResolver
+{
+    /**
+     * @var SearchService
+     */
+    private $searchService;
+
+    public function __construct(SearchService $searchService)
+    {
+        $this->searchService = $searchService;
+    }
+
+    /**
+     * @param $args
+     * @return Connection
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function searchContent($args)
+    {
+        $queryArg = $args['query'];
+
+        $query = new Query();
+        $criteria = [];
+
+        if (isset($queryArg['ContentTypeIdentifier'])) {
+            $criteria[] = new Query\Criterion\ContentTypeIdentifier($queryArg['ContentTypeIdentifier']);
+        }
+
+        if (isset($queryArg['Text'])) {
+            foreach ($queryArg['Text'] as $text) {
+                $criteria[] = new Query\Criterion\FullText($text);
+            }
+        }
+
+        if (count($criteria) === 0) {
+            return null;
+        }
+        $query->filter = count($criteria) > 1 ? new Query\Criterion\LogicalAnd($criteria) : $criteria[0];
+        $searchResult = $this->searchService->findContentInfo($query);
+
+        $contentItems = array_map(
+            function (SearchHit $hit) {
+                return $hit->valueObject;
+            },
+            $searchResult->searchHits
+        );
+
+        $connection = ConnectionBuilder::connectionFromArraySlice(
+            $contentItems,
+            $args,
+            [
+                'sliceStart' => 0,
+                'arrayLength' => $searchResult->totalCount,
+            ]
+        );
+        $connection->sliceSize = count($contentItems);
+
+        return $connection;
+    }
+}

crevillo/ezplatform-graphql

src/GraphQL/Relay/NodeResolver.php
@@ -1,0 +1,76 @@
+<?php
+
+namespace EzSystems\EzPlatformGraphQL\GraphQL\Relay;
+
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use EzSystems\EzPlatformGraphQL\Schema\Domain\Content\NameHelper;
+use Overblog\GraphQLBundle\Relay\Node\GlobalId;
+use Overblog\GraphQLBundle\Resolver\TypeResolver;
+
+class NodeResolver
+{
+    /**
+     * @var ContentService
+     */
+    private $contentService;
+
+    /**
+     * @var TypeResolver
+     */
+    private $typeResolver;
+
+    /**
+     * @var ContentTypeService
+     */
+    private $contentTypeService;
+
+    /**
+     * @var NameHelper
+     */
+    private $nameHelper;
+
+    public function __construct(ContentService $contentService, TypeResolver $typeResolver, ContentTypeService $contentTypeService, NameHelper $nameHelper)
+    {
+        $this->contentService = $contentService;
+        $this->typeResolver = $typeResolver;
+        $this->contentTypeService = $contentTypeService;
+        $this->nameHelper = $nameHelper;
+    }
+
+    /**
+     * @param $globalId
+     *
+     * @return null|\eZ\Publish\API\Repository\Values\Content\ContentInfo
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function resolveNode($globalId)
+    {
+        $params = GlobalId::fromGlobalId($globalId);
+
+        if (in_array($params['type'], ['Content', 'DomainContent'])) {
+            return $this->contentService->loadContentInfo($params['id']);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $object
+     *
+     * @return \GraphQL\Type\Definition\Type
+     */
+    public function resolveType($object)
+    {
+        if ($object instanceof ContentInfo) {
+            return $this->typeResolver->resolve(
+                $this->nameHelper->domainContentName(
+                    $this->contentTypeService->loadContentType($object->contentTypeId)
+                )
+            );
+        }
+    }
+}

src/GraphQL/Relay/SearchResolver.php
@@ -1,0 +1,70 @@
+<?php
+namespace EzSystems\EzPlatformGraphQL\GraphQL\Relay;
+
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
+use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
+use Overblog\GraphQLBundle\Relay\Connection\Output\ConnectionBuilder;
+
+class SearchResolver
+{
+    /**
+     * @var SearchService
+     */
+    private $searchService;
+
+    public function __construct(SearchService $searchService)
+    {
+        $this->searchService = $searchService;
+    }
+
+    /**
+     * @param $args
+     * @return Connection
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function searchContent($args)
+    {
+        $queryArg = $args['query'];
+
+        $query = new Query();
+        $criteria = [];
+
+        if (isset($queryArg['ContentTypeIdentifier'])) {
+            $criteria[] = new Query\Criterion\ContentTypeIdentifier($queryArg['ContentTypeIdentifier']);
+        }
+
+        if (isset($queryArg['Text'])) {
+            foreach ($queryArg['Text'] as $text) {
+                $criteria[] = new Query\Criterion\FullText($text);
+            }
+        }
+
+        if (count($criteria) === 0) {
+            return null;
+        }
+        $query->filter = count($criteria) > 1 ? new Query\Criterion\LogicalAnd($criteria) : $criteria[0];
+        $searchResult = $this->searchService->findContentInfo($query);
+
+        $contentItems = array_map(
+            function (SearchHit $hit) {
+                return $hit->valueObject;
+            },
+            $searchResult->searchHits
+        );
+
+        $connection = ConnectionBuilder::connectionFromArraySlice(
+            $contentItems,
+            $args,
+            [
+                'sliceStart' => 0,
+                'arrayLength' => $searchResult->totalCount,
+            ]
+        );
+        $connection->sliceSize = count($contentItems);
+
+        return $connection;
+    }
+}